7d38a6a8321b3c9e92c795e5927ffed793a62ab9
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-07 11:33:15 -0600

    implemented override method to add mongodb credentials

--- src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
+++ src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
@@ -1,10 +1,17 @@
 package guru.springframework.spring6reactivemongo.config;
 
+import com.mongodb.MongoClientSettings;
+import com.mongodb.MongoCredential;
+import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+import java.util.Collections;
+
+import static java.util.Collections.singletonList;
 
 /**
  * Created By dhaval on 2023-07-07
@@ -20,5 +27,16 @@ public class MongoConfig extends AbstractReactiveMongoConfiguration {
     @Override
     protected String getDatabaseName() {
         return "sfg";
+    }
+
+    @Override
+    protected void configureClientSettings(MongoClientSettings.Builder builder) {
+        builder.credential(MongoCredential.createCredential("root",
+                        "admin", "example".toCharArray()))
+                .applyToClusterSettings(settings -> {
+                    settings.hosts((singletonList(
+                            new ServerAddress("127.0.0.1", 27017)
+                    )));
+                });
     }
 }
