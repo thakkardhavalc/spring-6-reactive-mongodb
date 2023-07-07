d75aca55e0288a067792bf6e6fa673def6bb8e7c
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-07 11:27:57 -0600

    added mongoDb configs

---
+++ src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6reactivemongo.config;
+
+import com.mongodb.reactivestreams.client.MongoClient;
+import com.mongodb.reactivestreams.client.MongoClients;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
+
+/**
+ * Created By dhaval on 2023-07-07
+ */
+@Configuration
+public class MongoConfig extends AbstractReactiveMongoConfiguration {
+
+    @Bean
+    public MongoClient mongoClient() {
+        return MongoClients.create();
+    }
+
+    @Override
+    protected String getDatabaseName() {
+        return "sfg";
+    }
+}
