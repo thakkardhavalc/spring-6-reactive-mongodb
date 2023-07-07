f1453b036439ea42ea97beb54ed554054c4e23dc
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-07 10:57:48 -0600

    intial commit

---
+++ src/test/java/guru/springframework/spring6reactivemongo/Spring6ReactiveMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring6reactivemongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6ReactiveMongoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
