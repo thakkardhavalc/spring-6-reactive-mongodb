36373db86860e76abf4c19c7b520b383d97d907b
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-11 10:44:54 -0600

    implemented database initialization

--- src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
+++ src/main/java/guru/springframework/spring6reactivemongo/config/MongoConfig.java
@@ -1,17 +1,10 @@
 package guru.springframework.spring6reactivemongo.config;
 
-import com.mongodb.MongoClientSettings;
-import com.mongodb.MongoCredential;
-import com.mongodb.ServerAddress;
 import com.mongodb.reactivestreams.client.MongoClient;
 import com.mongodb.reactivestreams.client.MongoClients;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.config.AbstractReactiveMongoConfiguration;
-
-import java.util.Collections;
-
-import static java.util.Collections.singletonList;
 
 /**
  * Created By dhaval on 2023-07-07
