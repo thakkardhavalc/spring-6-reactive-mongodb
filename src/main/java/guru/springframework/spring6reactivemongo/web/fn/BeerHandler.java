aa717e52a054286540c90039a98c215b9302bcf6
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-11 11:24:07 -0600

    added getById in handler class and verified the logic by implementing the test case

--- src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
+++ src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
@@ -17,6 +17,11 @@ public class BeerHandler {
 
     private final BeerService beerService;
 
+    public Mono<ServerResponse> getBeerById(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.getById(request.pathVariable("beerId")), BeerDTO.class);
+    }
+
     public Mono<ServerResponse> listBeers(ServerRequest request) {
         return ServerResponse.ok()
                 .body(beerService.listBeers(), BeerDTO.class);
