59f7725cfa88a9d5c23a690f75daa4ce8a262003
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-11 10:57:01 -0600

    implemented web flux handler

---
+++ src/main/java/guru/springframework/spring6reactivemongo/web/fn/BeerHandler.java
@@ -0,0 +1,24 @@
+package guru.springframework.spring6reactivemongo.web.fn;
+
+import guru.springframework.spring6reactivemongo.model.BeerDTO;
+import guru.springframework.spring6reactivemongo.services.BeerService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+import org.springframework.web.reactive.function.server.ServerRequest;
+import org.springframework.web.reactive.function.server.ServerResponse;
+import reactor.core.publisher.Mono;
+
+/**
+ * Created By dhaval on 2023-07-11
+ */
+@Component
+@RequiredArgsConstructor
+public class BeerHandler {
+
+    private final BeerService beerService;
+
+    public Mono<ServerResponse> listBeers(ServerRequest request) {
+        return ServerResponse.ok()
+                .body(beerService.listBeers(), BeerDTO.class);
+    }
+}
