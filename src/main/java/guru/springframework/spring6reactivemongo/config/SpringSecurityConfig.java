b975be43360abf98f14483bab924acc0ba27e8a5
thakkardhavalc <thakkardhavalc@gmail.com>
2023-07-13 10:41:18 -0600

    added oauth2 config and property

---
+++ src/main/java/guru/springframework/spring6reactivemongo/config/SpringSecurityConfig.java
@@ -0,0 +1,28 @@
+package guru.springframework.spring6reactivemongo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+/**
+ * Created By dhaval on 2023-07-13
+ */
+@Configuration
+@EnableWebFluxSecurity
+public class SpringSecurityConfig {
+
+    @Bean
+    SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+        http.csrf().disable()
+                .authorizeExchange()
+                .anyExchange()
+                .authenticated()
+                .and()
+                .oauth2ResourceServer()
+                .jwt();
+
+        return http.build();
+    }
+}
